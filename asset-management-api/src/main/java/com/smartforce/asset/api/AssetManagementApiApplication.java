9e5f20b7d7964264bd191085f1e8b3d476ac555b
devvicha <160200393+devvicha@users.noreply.github.com>
2024-12-17 11:38:17 +0530

    services for leave and assest managment

---
+++ asset-management-api/src/main/java/com/smartforce/asset/api/AssetManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartforce.asset.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementApiApplication.class, args);
+	}
+
+}
---
+++ leave-management-api/src/main/java/com/smartforce/leave_management_api/Service/Impl/LeavingServiceImpl.java
@@ -0,0 +1,57 @@
+package com.smartforce.leave_management_api.Service.Impl;
+
+import com.smartforce.leave_management_api.Model.Leave;
+import com.smartforce.leave_management_api.Repository.LeaveRepository;
+import com.smartforce.leave_management_api.Service.LeavingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.management.Query;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class LeavingServiceImpl implements LeavingService {
+
+    @Autowired
+    private LeaveRepository leaveRepository;
+
+    public List<Leave> getAllLeaves(){
+        return leaveRepository.findAll();
+    }
+
+    public Leave addLeave(Leave leave){
+        return leaveRepository.save(leave);
+    }
+
+    public Leave updateLeave(Leave leave, String id){
+        Leave existingLeave = leaveRepository.findById(id).orElseThrow(() -> new RuntimeException());
+        existingLeave.setEmployeeName(leave.getEmployeeName());
+        existingLeave.setLeaveType(leave.getLeaveType());
+        existingLeave.setPin(leave.getPin());
+        existingLeave.setApplyDate(leave.getApplyDate());
+        existingLeave.setStartDate(leave.getStartDate());
+        existingLeave.setEndDate(leave.getEndDate());
+        leaveRepository.save(existingLeave);
+        return existingLeave;
+    }
+
+    public void deleteLeave(String id){
+        leaveRepository.findById(id).orElseThrow(() -> new RuntimeException());
+        leaveRepository.deleteById(id);
+    }
+
+    public List<Leave> getLeavesByEmployeeNameAndStatus(String employeeName, LocalDateTime startDate, LocalDateTime endDate, String status){
+        return leaveRepository.findByEmployeeNameAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndStatus(employeeName,startDate,endDate,status);
+    }
+
+
+    public Leave approveLeaveRequest(String id){
+        Leave leave = leaveRepository.findById(id).orElseThrow(() -> new RuntimeException());
+        leave.setStatus("Approved");
+        return leaveRepository.save(leave);
+    }
+
+}
